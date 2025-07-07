045f17b12e97210e4d1f8883bc9fa0de34506d15
Joseph <joseph-tds@mail.ru>
2025-07-07 22:56:20 +0500

    Added requestLogger, unknownEndpoint, errorHandler middlewares.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const blogsRouter = require('./controllers/blogs');
 const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
 const app = express();
 
@@ -19,8 +20,11 @@ mongoose
     })
 
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
